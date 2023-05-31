76a30956e68ec22040f9427a450c7f9c1e04095c
salomonsa <131862467+salomonsa@users.noreply.github.com>
2023-05-31 15:25:21 +0200

    Update vgenerator.py

--- vgenerator.py
+++ vgenerator.py
@@ -8,7 +8,7 @@ from PIL import Image
 
 def generate():
 
-    change_settings(    {"IMAGEMAGICK_BINARY": r"C:/Program Files/ImageMagick-7.1.1-Q16-HDRI/magick.exe"}) 
+    change_settings({"IMAGEMAGICK_BINARY":r"c:\Program Files\ImageMagick"})
     
     images = []
     captions = []
